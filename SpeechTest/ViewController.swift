19858dc95341b67d1f194cad259828a9677aaff1
shangkun <wushangkun@chelun.com>
2019-03-13 19:39:33 +0800

    initial demo

---
+++ SpeechTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechTest
+//
+//  Created by shangkun on 2019/3/13.
+//  Copyright © 2019年 wushangkun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
